52fb6a545eb809fe5e925bbd1948a00aeaec0c4a
Nure <nalamsiddiq@gmail.com>
2024-12-27 21:43:30 +0600

    Add membership operators

---
+++ mermbership_operators.py
@@ -0,0 +1,10 @@
+# in, not in
+
+print ('siddiq' in "nure alam siddiq") # True
+print ('siddiq' in "nure alam Siddiq") # False
+
+print ('ramesh' not in "nure alam Siddiq")
+
+fruits = ['orange', 'apple', 'banana']
+
+print('pinnanle' in fruits)
