e90f111347804013bb3d27c0462c27939c54ff7b
Nure <nalamsiddiq@gmail.com>
2024-12-27 22:18:07 +0600

    Add conditions

---
+++ conditions.py
@@ -0,0 +1,25 @@
+# if, elif, else
+
+# temperature = 22
+
+# indentation
+# if temperature > 25:
+#     print("It's hot today")
+# else:
+#     print('comfortable weather')
+
+
+
+temperature = 19
+
+if temperature > 40:
+    print("It's very hot today, don't go outside")
+
+elif 30 <= temperature <= 40:
+    print("It's worm weather, wear light clother")
+
+elif 20 <= temperature < 30:
+    print("It's a confortable weather, enjoy your day")
+
+else:
+    print('very cold in the outside, wear a jacket')
