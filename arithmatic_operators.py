c6869cdd8265c7a957d091cec7de2500a79936a6
Nure <nalamsiddiq@gmail.com>
2024-12-27 21:21:30 +0600

    Add arithmatical operators

---
+++ arithmatic_operators.py
@@ -0,0 +1,11 @@
+a = 5 #integer (int)
+b = 3 #integer (int)
+c = 3.52306645 #decimal (float)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b) #mod, modulus
+print(a ** b) #Expo, power
